519a2b0aa5d7165dc72be6cc5af03089ccdbdd7b
RonRolle322 <ron.rolle@me.com>
2021-01-28 18:33:25 +0100

    add framework

---
+++ MailCheckerFramework/MailCheckerFramework/MailCheckerFramework.h
@@ -0,0 +1,18 @@
+//
+//  MailCheckerFramework.h
+//  MailCheckerFramework
+//
+//  Created by Ron Rolle on 28.01.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MailCheckerFramework.
+FOUNDATION_EXPORT double MailCheckerFrameworkVersionNumber;
+
+//! Project version string for MailCheckerFramework.
+FOUNDATION_EXPORT const unsigned char MailCheckerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MailCheckerFramework/PublicHeader.h>
+
+
